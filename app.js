866994f22163b17242ba43351d094a20c7b6647f
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-11-03 22:37:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1027,7 +1027,7 @@ client.on('ready',async () => {
   console.log(client.user.username);
   try {
     const config = {
-      name: "❊ ◆Rainbow◆", // اسم الرتبة
+      name: "Rainbow", // اسم الرتبة
       guildid: "507868593103568901", // اي دي السيرفر
       sec: 0.3 // عدد الثواني
     };
