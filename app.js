ea6b5f9dc2324ca40b4530cfac4660cda2c2a3d7
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-11-04 13:45:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1052,7 +1052,7 @@ client.on('ready',async () => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "ArabicoBot Support";
+    let copy = "GLaD SysTeM";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
